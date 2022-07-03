5137d02975b6d1b7c1386d02ee04e1f0803310a2
konnotakafumi4 <longwenjinye@gmail.com>
2022-07-03 05:27:09 +0000

    [Fix]プロフィール編集成功した後　users_controllerを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books #bookモデルとバリデーションが逆
+  has_many :books, dependent: :destroy #bookモデルとバリデーションが逆
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 
-  
-  
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
