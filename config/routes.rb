372a4088049ae7b436d81658595716b40108c3f6
konnotakafumi4 <longwenjinye@gmail.com>
2022-07-04 02:31:52 +0000

    [Finish]フォローフォロワー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root :to =>"homes#top"
-  get "home/about"=>"homes#about", as: 'about'
-  #devise_for :usersをresourcesの上に移動
   devise_for :users
+  get "home/about"=>"homes#about", as: 'about'
+
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:new, :create, :index, :show, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #end追加
+
 end
